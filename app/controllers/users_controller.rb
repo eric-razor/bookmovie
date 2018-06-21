c78ac2003c1f5adf3da3d69ef18eff59bdfc49cf
github username <github email address>
2018-06-21 14:04:28 -0400

    added login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,19 +4,29 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create #creates a new user
+    @user = User.new(user_params)
+
+    if @user.valid?
+      @user.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+
   def show  #show page for the users profile
     @user = User.find(params[:id])
   end
 
-  def create #creates a new user
-    @user = User.create(user_params)
-  end
+
 
 
   private
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:name, :email, :password)
   end
 
 end
